b2059d1efd7d06a5c489f7736c77b885a1a46cdc
TedApel <TedApel@users.noreply.github.com>
2026-03-24 16:42:44 +0000

    Deploying to gh-pages from @ TedApel/tedapel.github.io@9059ced8a3cf83961f3492652c14d1d828c7d840 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -750,6 +750,11 @@ ninja.data = [{
           description: "A sound installation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/fororganpipes/";
+            },},{id: "projects-coupled-resonance",
+          title: 'Coupled Resonance',
+          description: "Series of interdependent resonant sound works",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/coupledresonance/";
             },},{id: "projects-negated-sound",
           title: 'Negated Sound',
           description: "Sound Analysis synthesis method",
@@ -778,12 +783,7 @@ ninja.data = [{
             },},{id: "projects-boise-state-university-faculty-artist-series-recital-dr-kramer-milan-percussion-dr-ted-apel-electronics-friday-april-18-2025-at-7-30-pm-morrison-center-recital-hall-program-variations-on-the-mystery-sonata-s-passacaglia-heinrich-biber-1604-1744-arr-ted-apel-and-kramer-milan-mvmt-1-burst-from-the-mathematics-of-resonant-bodies-john-luther-adams-b-1953-to-a-landscape-ted-apel-and-kramer-milan-in-a-landscape-john-cage-1912-1992-arr-ted-apel-and-kramer-milan-faculty-artist-series-recital",
           title: 'Boise State University — Faculty Artist Series Recital Dr. Kramer Milan, percussion Dr....',
           description: "",
-          section: "Projects",},{id: "projects-coupled-resonance",
-          title: 'Coupled Resonance',
-          description: "Series of interdependent resonant sound works",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/coupledresonance/";
-            },},{
+          section: "Projects",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
